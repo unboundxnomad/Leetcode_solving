5eaf46572e1116e069411a09503137635b35efa8
unboundx <unboundxnomad@gmail.com>
2026-07-20 13:52:01 +0530

    Time: 31 ms (21.44%), Space: 18.1 MB (13.76%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,12 +1,14 @@
 class Solution(object):
     def groupAnagrams(self, strs):
-        res = defaultdict(list)
+        res= defaultdict(list)
         for s in strs:
             count=[0]*26
             for c in s:
                 count[ord(c)-ord('a')]+=1
             res[tuple(count)].append(s)
         return res.values()
-            
 
+
+            
+        
         
