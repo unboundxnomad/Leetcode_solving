cb54f19dc28c8e8cd63f95380234af3023da9b04
unboundx <unboundxnomad@gmail.com>
2026-07-28 11:04:49 +0530

    Time: 0 ms (100%), Space: 12.9 MB (81.83%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,19 @@
 class Solution(object):
     def twoSum(self, nums, target):
         prev_map={}
-        for i,n in enumerate(nums):
-            diff=target-n
+        for i , n in enumerate(nums):
+            diff= target-n
             if diff in prev_map:
                 return [prev_map[diff],i]
             prev_map[n]=i
+
+       
+
+        
+
+
+
+        
+            
+        
         
